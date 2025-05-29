ed2d0083bd5003ec6b7310ca6681fdfb041fd432
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-05-29 13:04:53 +0900

    確認課題完了

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -90,7 +90,7 @@ void GameScene::EffectBorn(KamataEngine::Vector3 position) {
 		Vector3 position_ = position;
 
 		float size = abs(distribution(randomEngine) * 5.0f);
-		float rotation = (distribution(randomEngine) * 6.0f);
+		float rotation = (distribution(randomEngine) * 3.0f);
 
 
 		// 移動量
@@ -104,10 +104,9 @@ void GameScene::EffectBorn(KamataEngine::Vector3 position) {
 		float r = static_cast<float>(rand()) / RAND_MAX;
 		float g = static_cast<float>(rand()) / RAND_MAX;
 		float b = static_cast<float>(rand()) / RAND_MAX;
-		float a = 1.0f; // 不透明
 
 		// 色をランダムに設定
-		effect->color_ = {r, g, b, a};
+		effect->color_ = {r, g, b};
 
 	}
 }
