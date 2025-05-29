d45d7d7891ea79ad72d7b8b173344f39296c3a04
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-05-29 11:35:18 +0900

    確認課題完了

--- DirectXGame/Effect.cpp
+++ DirectXGame/Effect.cpp
@@ -26,7 +26,7 @@ void Effect::Initialize(Model* model, Vector3 position, Vector3 velocity,float s
 	velocity_ = velocity;
 
 	// 大きさ
-	worldTransform_.scale_ = {size, 0.5f, 0.5f};
+	worldTransform_.scale_ = {size, 0.5f, 1.0f};
 
 	// 回転角度
 	worldTransform_.rotation_ = {0.0f, 0.0f, rotation};
